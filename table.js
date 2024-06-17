e7d986b9cb94d34e42125233b115a5d5db7653c3
Yury Oberyukhtin <oberset@mail.ru>
2024-06-17 03:08:15 +0300

    Update table.js

--- table.js
+++ table.js
@@ -3,23 +3,10 @@ function table() {
     const list = container.querySelectorAll('span, .zero');
 
     for (let element of list) {
-        element.addEventListener('touchstart', function (e) {
-            if (e.touches.length === 1) {
-                if (!expired) {
-                    expired = e.timeStamp + 400
-                } else if (e.timeStamp <= expired) {
-                    // remove the default of this event ( Zoom )
-                    e.preventDefault()
-                    const n = parseInt(e.target.textContent);
-                    if (!isNaN(n)) {
-                        addNumber(n);
-                    }
-                    // then reset the variable for other "double Touches" event
-                    expired = null
-                } else {
-                    // if the second touch was expired, make it as it's the first
-                    expired = e.timeStamp + 400
-                }
+        element.addEventListener('click', function(e) {
+            const n = parseInt(e.target.textContent);
+            if (!isNaN(n)) {
+                addNumber(n);
             }
         })
     }
