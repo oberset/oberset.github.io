16cb784eb3b837af0624fd0cbc721768b86e5e89
Yury Oberyukhtin <oberset@mail.ru>
2024-06-17 13:40:39 +0300

    Update table.js

--- table.js
+++ table.js
@@ -1,9 +1,11 @@
+const EVENT_TYPE = 'click';
+
 function table() {
     const container = document.querySelector('.table');
     const list = container.querySelectorAll('span, .zero');
 
     for (let element of list) {
-        element.addEventListener('click', (e) => {
+        element.addEventListener(EVENT_TYPE, (e) => {
             const n = parseInt(e.target.textContent);
             if (!isNaN(n)) {
                 addNumber(n);
@@ -11,3 +13,4 @@ function table() {
         });
     }
 }
+
