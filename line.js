9dac32fcb59452adda7e83de21cb5ead4215ffe6
Yury Oberyukhtin <oberset@mail.ru>
2024-08-27 19:54:06 +0300

    Update line.js

--- line.js
+++ line.js
@@ -163,13 +163,14 @@ class Bets {
         }
 
         this.hotNumbersList = this.hotNumbersList.slice(0, this.numbersCount);
+        console.log('hotNumbersList', this.hotNumbersList);
 
         this.bets.clear();
 
         for (let nextNumber of this.hotNumbersList) {
             this.bets.set(nextNumber, 1);
         }
-
+        console.log('Bets', this.bets.entries());    
         return this.bets.entries();
     }
 
