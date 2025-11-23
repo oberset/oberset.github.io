747f759ba9e88bc0865cb44ad8baac19acce4945
Yury Oberyukhtin <oberset@mail.ru>
2025-11-24 01:29:09 +0300

    Update numbers-position.js

--- numbers-position.js
+++ numbers-position.js
@@ -3,7 +3,7 @@ function calcNumbersPosition(lineStructure, repeats) {
     const items = lineStructure.getItemsElement(group);
     const template = lineStructure.getFirstItemElement(items);
 
-    const [hotAvg, coldAvg] = getAvgHotColdRepeats(repeats);
+    const [hotAvg, coldAvg] = getAvgRepeats(repeats);
     const text = [hotAvg, coldAvg].join(' / ');
 
     const item = createHTMLElement(text, (value) => createItem(template, value));
